8913c3d43d32aac98e662b1c264adb222033dd2b
Marcus Phillipe <67166960+marcusphillipe@users.noreply.github.com>
2025-08-08 10:32:48 -0300

    Implemented CI

--- api-ui-tests/cypress.config.js
+++ api-ui-tests/cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('cypress')
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'http://localhost:3000'
+        baseUrl: 'http://localhost:3000',
+        specPattern: 'tests/ui/e2e/**/*.cy.js',
+        supportFile: 'cypress/support/e2e.js',
     }
 })
